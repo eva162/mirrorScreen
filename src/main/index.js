7bb0be4c84c0fe942e26759e35483a07f7e3aa0d
eva162 <rex162@qq.com>
2019-05-01 12:36:22 +0800

    修改

--- src/main/index.js
+++ src/main/index.js
@@ -19,9 +19,9 @@ const winURL = process.env.NODE_ENV === 'development'
 
 function createWindow() {
 
-   /**
-   * Initial window options
-   */
+  /**
+  * Initial window options
+  */
   mainWindow = new BrowserWindow({
     height: 1000,
     useContentSize: true,
@@ -50,12 +50,12 @@ function createWindow() {
     let base64 = buffer.toString("base64")
     controlWindow.webContents.send('image', base64)
     let content = Buffer.allocUnsafe(8)
-    content.writeInt16BE(dirty.x,0)
-    content.writeInt16BE(dirty.y,2)
-    content.writeInt16BE(dirty.width,4)
-    content.writeInt16BE(dirty.height,6)
-    content = Buffer.concat([content,buffer]);
-    let rdata = CreateRequestBuffer(3,content)
+    content.writeInt16BE(dirty.x, 0)
+    content.writeInt16BE(dirty.y, 2)
+    content.writeInt16BE(dirty.width, 4)
+    content.writeInt16BE(dirty.height, 6)
+    content = Buffer.concat([content, buffer]);
+    let rdata = CreateRequestBuffer(3, content)
     websocket.send(rdata)
 
     console.log('data send')
@@ -90,7 +90,15 @@ websocket.on('open', function () {
   websocket.send(rdata)
 });
 
+const MOUSETYPE = {
+  "0":"mouseDown",
+  "1":"mouseMove",
+  "2":"mouseUp",
+  "3":"mouseOver",
+}
+
 websocket.on('message', function (data) {
+  const CONTENTOFFSET = 8;
   //console.log(data)
   if (data &&
     Buffer.isBuffer(data) &&
@@ -104,12 +112,26 @@ websocket.on('message', function (data) {
         let rdata = CreateRequestBuffer(1)
         websocket.send(rdata)
         break;
+      case 2:
+        let arg = {
+          type: MOUSETYPE[data[CONTENTOFFSET+6]],
+          x: data.readInt16BE[CONTENTOFFSET],
+          y: data.readInt16BE[CONTENTOFFSET+2],
+          button: data.readInt16BE[CONTENTOFFSET+8] == 0 ? 'left' : 'right',
+          clickCount: 1
+        }
+        mainWindow.webContents.sendInputEvent(arg)
+        break;
+      default:
+        console.log('message default')
+        break;
 
     }
 
   }
 
 });
+
 
 //创建报文
 function CreateRequestBuffer(comm, content) {
@@ -120,8 +142,8 @@ function CreateRequestBuffer(comm, content) {
   let cnt = Buffer.from(conterHex, "hex")
   rdata[1] = comm
   cnt.copy(rdata, 2, cnt.length - 2, cnt.length)
-  if(content && content.length > 0){
-    rdata = Buffer.concat([rdata,content]);
+  if (content && content.length > 0) {
+    rdata = Buffer.concat([rdata, content]);
   }
   conter++
   return rdata
